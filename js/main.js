5a2ca1af6fc5ec903a94dbf7e969134bad7ff3c4
Jacky Liang <jackyliang@cmu.edu>
2019-03-08 14:22:35 -0500

    lab dashboard init

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.twttr = (function (d,s,id) {
+
+    var t, js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+    js.src="https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+
+}(document, "script", "twitter-widget-0"));
+
+twttr.ready(function (twttr) {
+
+    twttr.widgets.load();
+    setInterval(function() {
+        twttr.widgets.load();
+        console.log("update twitter timeline each second");
+    }, 1000);
+
+});
